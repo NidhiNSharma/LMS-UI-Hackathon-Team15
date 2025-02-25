6a4ef97062415d044d3ed97101d0842de2364dce
rathireshu <rathi.reshu@gmail.com>
2025-02-25 17:56:28 -0500

    final commit

--- lmsUI/src/test/java/testRunner/TestRun.java
+++ lmsUI/src/test/java/testRunner/TestRun.java
@@ -2,10 +2,13 @@
 package testRunner;
 
 import io.cucumber.testng.CucumberOptions;
+
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-					features="src/test/resources/FeaturesFiles/ClassPageFeatures/TC401_ClassPageValidation",
+					features="src/test/resources/FeaturesFiles/",
 					glue={"stepDefinitions","hooks"},
 					//tags="@login and @home and @program",
 					monochrome=true,
@@ -18,16 +21,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 public class TestRun extends AbstractTestNGCucumberTests{
 
 	
-/*	@Override
-    
-	/*@Override
->>>>>>> 2eddbd7f7b39f0fa73eb825d9528c41456ac7435
+    @Override
 	@DataProvider(parallel=true)
 
 	public Object[][] scenarios(){
 		
 		return super.scenarios();
-
 	
-}*/
+}
 }
